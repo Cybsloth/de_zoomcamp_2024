49275771a801e31df3b8091ed369b5d9f8f14f02
Alexander Kirilin <adkirilin@gmail.com>
2024-03-09 21:46:41 +0500

    Workshop 2: RisingWave

---
+++ workshop_2/sql/question_0.sql
@@ -0,0 +1,16 @@
+CREATE MATERIALIZED VIEW latest_dropoff_time AS
+    WITH t AS (
+        SELECT MAX(tpep_dropoff_datetime) AS latest_dropoff_time
+        FROM trip_data
+    )
+    SELECT taxi_zone.Zone as taxi_zone, latest_dropoff_time
+    FROM t,
+            trip_data
+    JOIN taxi_zone
+        ON trip_data.DOLocationID = taxi_zone.location_id
+    WHERE trip_data.tpep_dropoff_datetime = t.latest_dropoff_time;
+
+--    taxi_zone    | latest_dropoff_time
+-- ----------------+---------------------
+--  Midtown Center | 2022-01-03 17:24:54
+-- (1 row)
